3f0cb29c72fac7713f69e00cfe5da5ab9c624a35
Linus Castelino <castelinolinus@gmail.com>
2019-04-11 01:54:31 -0400

    Added endpoint browse buttons

--- src/app/apicalls.service.ts
+++ src/app/apicalls.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 import { ILoginResponse } from './models/ILoginResponse';
 
 import{ IUser } from './models/IUser';
-const endpoint = 'http://10.84.39.155:8080';
+const endpoint = 'http://192.168.1.226:8080';
 
 
 const context = endpoint + '/api/stork';
